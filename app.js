f4051fb87a3f12ae580227e2ed6c3ef586b3df85
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-14 23:10:22 +0100

    feat: add Electron dev dep, create Electron entry point file

---
+++ app.js
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron/main";
+import { join } from "node:path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile(
+    join(__dirname, "/dist/angular-electron-starter/browser/index.html")
+  );
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
