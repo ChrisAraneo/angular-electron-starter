bb8672c5f85408e02b32f0103ae5c7ffa610f5e3
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-14 23:28:28 +0100

    feat: migrate to standalone moduleless app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: false,
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet],
+  standalone: true,
 })
 export class AppComponent {
   title = 'angular-electron-starter';
