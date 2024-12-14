4d61189099a44eb48c7eae8c79546594e0bf9155
Krzysztof Pająk <chris.araneo@gmail.com>
2024-12-14 23:18:22 +0100

    style: format code

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-import { app, BrowserWindow } from "electron/main";
-import { join } from "node:path";
+import { app, BrowserWindow } from 'electron/main';
+import { join } from 'node:path';
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, "preload.js"),
+      preload: join(__dirname, 'preload.js'),
     },
   });
 
   win.loadFile(
-    join(__dirname, "/dist/angular-electron-starter/browser/index.html")
+    join(__dirname, '/dist/angular-electron-starter/browser/index.html'),
   );
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
